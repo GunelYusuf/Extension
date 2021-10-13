c7ea5dc09a6cca31bae85f059e07d790bd258baa
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2021-10-14 03:42:43 +0400

    extension Creat

---
+++ ExtensionTask/Extension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtensionTask
+{
+    static class Extension
+    {
+        public static int MatchCount(this string msg, string pattern)
+        {
+            int Count = 0;
+            MatchCollection FindWord = Regex.Matches(msg, @"\b(\w*Gunel\w*)\b");
+            foreach (var i in FindWord)
+            {
+                Count++;
+            }
+            return Count;
+        }
+
+
+        public static void Polendrom(this int x)
+        {
+            int Reverse = 0;
+
+            for (int i = x; i > 0; i/= 10)
+            {
+               Reverse =(Reverse * 10) + i % 10;
+            }
+            if (x == Reverse)
+            {
+                Console.WriteLine($"{x} Polendrom'dur");
+
+            }
+            else
+            {
+                Console.WriteLine($"{x} Polendrom deyil");
+            }
+
+        }
+
+
+    }
+}
---
+++ ExtensionTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtensionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Salam Gunel,Necesen Gunel,hardasan Gunel";
+            Console.WriteLine(msg.MatchCount("Gunel"));
+
+            Console.Write("Reqem daxil edin: ");
+            int x = int.Parse(Console.ReadLine());
+            x.Polendrom();
+           
+           
+            
+
+        }
+    }
+}
